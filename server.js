c83360b590cadf1a673e0d65d3b072e22d2848d4
Cihan <cihangonen35@gmail.com>
2021-04-19 18:39:35 +0300

    deploy

--- server.js
+++ server.js
@@ -18,13 +18,11 @@ const route = require("./router/exercise");
 
 app.use("/", route);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client", "build"));
+});
 
 //database connection
 mongoose.connect(dbURI, {
