1fc7105fffca75c8a32a4421319f386b9a065154
Cihan <cihangonen35@gmail.com>
2021-04-19 23:24:45 +0300

    serverjs for db

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+//database connection
+mongoose.connect(process.env.MONGODB_URI || dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("./client/build"));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`));
@@ -18,15 +29,8 @@ const route = require("./router/exercise");
 
 app.use("/", route);
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client", "build"));
-});
-
-//database connection
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client", "build"));
+// });
